231ccb9d7d14c8db1496dcc893003f2b1d08414c
brunoramossilva <brunogabriel0900@gmail.com>
2025-03-26 22:08:50 -0300

    Att.

--- animal.py
+++ animal.py
@@ -1,13 +1,31 @@
 class Animal:
+
+    animais = []
+
     def __init__(self, nome, raca):
         self.nome = nome
         self.raca = raca
-        self.vacinacao = False
+        self._vacinacao = False
+        Animal.animais.append(self)
 
-    def __str__(self):
-            return f'O nome do animal é {self.nome}, da raça {self.raca}.'
+    def listar_animais():
+        print('-' * 85)
+        print(f'| {'# Nome do Animal #'.ljust(25)} | {'# Raça do Animal #'.ljust(25)} | {'# Status de Vacinação #'.ljust(25)} |')
+        print('-' * 85)
+        for animal in Animal.animais:
+            print(f'| {animal.nome.ljust(25)} | {animal.raca.ljust(25)} | {animal.vacinacao.ljust(25)} |')
+            print('-' * 85)
+
+    @property
+    def vacinacao(self):
+        return 'Vacinado(a)' if self._vacinacao else 'Não Vacinado(a)'
+
+
+
 
 toby = Animal('Toby', 'Vira-lata')
+mel = Animal('Mel', 'Gata')
+Saddam = Animal('Saddam', 'Vira-lata')
+Pandinha = Animal('Pandinha', 'Chihuahua')
 
-print(toby)
-#
+Animal.listar_animais()
