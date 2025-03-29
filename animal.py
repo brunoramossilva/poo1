7c2aeb125cfe541a8ab4da82ac8a0586ea4bd515
brunoramossilva <brunogabriel0900@gmail.com>
2025-03-29 12:26:14 -0300

    Att.

--- animal.py
+++ animal.py
@@ -3,29 +3,34 @@ class Animal:
     animais = []
 
     def __init__(self, nome, raca):
-        self.nome = nome
-        self.raca = raca
+        self._nome = nome.title()
+        self._raca = raca.title()
         self._vacinacao = False
         Animal.animais.append(self)
 
-    def listar_animais():
+    @classmethod
+    def listar_animais(cls):
         print('-' * 85)
         print(f'| {'# Nome do Animal #'.ljust(25)} | {'# Raça do Animal #'.ljust(25)} | {'# Status de Vacinação #'.ljust(25)} |')
         print('-' * 85)
-        for animal in Animal.animais:
-            print(f'| {animal.nome.ljust(25)} | {animal.raca.ljust(25)} | {animal.vacinacao.ljust(25)} |')
+        for animal in cls.animais:
+            print(f'| {animal._nome.ljust(25)} | {animal._raca.ljust(25)} | {animal.vacinacao.ljust(25)} |')
             print('-' * 85)
 
     @property
     def vacinacao(self):
         return 'Vacinado(a)' if self._vacinacao else 'Não Vacinado(a)'
 
-
-
+def criando_animal():
+    animal = [animal.strip() for animal in input('Digite o nome e a raça do '
+    'animal separados por vírgula: ').split(',')]
+    return Animal(*animal)
 
 toby = Animal('Toby', 'Vira-lata')
 mel = Animal('Mel', 'Gata')
 Saddam = Animal('Saddam', 'Vira-lata')
+Saddam._vacinacao = True
 Pandinha = Animal('Pandinha', 'Chihuahua')
+criando_animal()
 
 Animal.listar_animais()
